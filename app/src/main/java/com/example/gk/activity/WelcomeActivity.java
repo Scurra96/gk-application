9a46959df307527bd66e89fb6896c6fdb7a14f4c
gopiskyb <gopiskyb1322@gmail.com>
2021-10-08 18:12:45 +0530

    GN Commits

--- app/src/main/java/com/example/gk/activity/WelcomeActivity.java
+++ app/src/main/java/com/example/gk/activity/WelcomeActivity.java
@@ -1,14 +1,16 @@
-package com.example.gk;
+package com.example.gk.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gk.R;
+
+public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_welcome);
     }
 }
