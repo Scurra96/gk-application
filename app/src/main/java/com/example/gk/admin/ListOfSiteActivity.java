72a8eef33eb69abb6da7fc9573e38a52a09d92bb
gopinathan <jokeratg96@gmail.com>
2021-10-10 17:51:12 +0530

    GN added new commit

---
+++ app/src/main/java/com/example/gk/admin/ListOfSiteActivity.java
@@ -0,0 +1,16 @@
+package com.example.gk.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gk.R;
+
+public class ListOfSiteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_site);
+    }
+}
