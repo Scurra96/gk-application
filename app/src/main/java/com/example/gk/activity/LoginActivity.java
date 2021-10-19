2ccf5c09f06f82c0268a3195a95eea363c20b6b0
gopinathan <jokeratg96@gmail.com>
2021-10-19 23:40:34 +0530

    GN added commit

--- app/src/main/java/com/example/gk/activity/LoginActivity.java
+++ app/src/main/java/com/example/gk/activity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.gk.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,17 +10,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.gk.HomeActivity;
 import com.example.gk.R;
 import com.example.gk.admin.AdminHomeActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     Button button_Login;
 //    SharedPreferences sharedPreferences;
     EditText editText_username,editText_password;
-    String username,password;
+    String username = "",password = "";
+    DatabaseReference databaseReference;
+    ValueEventListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +38,10 @@ public class LoginActivity extends AppCompatActivity {
         SharedPreferences pref = getApplicationContext().getSharedPreferences(
                 "MyPref", MODE_PRIVATE);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference("RegisterDetails");
+
         editText_username = findViewById(R.id.editText_username);
         editText_password = findViewById(R.id.editText_password);
-
-//         username = editText_username.getText().toString();
-//         password = editText_password.getText().toString();
 
         button_Login = findViewById(R.id.button_Login);
         button_Login.setOnClickListener(new View.OnClickListener() {
@@ -45,12 +53,43 @@ public class LoginActivity extends AppCompatActivity {
                     Intent i = new Intent(getApplicationContext(), AdminHomeActivity.class);
                     startActivity(i);
                 }else {
-                    Intent i = new Intent(getApplicationContext(), HomeActivity.class);
-                    SharedPreferences.Editor editor = pref.edit();
-                    editor.putString("USERNAME", editText_username.getText().toString());
-                    editor.apply();
-                    startActivity(i);
-                    finish();
+                    if(!editText_username.getText().toString().isEmpty()){
+                        username = editText_username.getText().toString();
+                        password = editText_password.getText().toString();
+                        databaseReference.child(username).addListenerForSingleValueEvent(listener);
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Enter fields", Toast.LENGTH_SHORT).show();
+                    }
+                    listener = new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.exists()){
+                                String firebase_password = snapshot.child("password")
+                                        .getValue(String.class);
+                                if(firebase_password.equalsIgnoreCase(password)){
+                                    Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                                    SharedPreferences.Editor editor = pref.edit();
+                                    editor.putString("USERNAME", editText_username.getText().toString());
+                                    editor.apply();
+                                    startActivity(i);
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(LoginActivity.this, "Wrong Password",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "No Record Found",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    };
                 }
             }
         });
